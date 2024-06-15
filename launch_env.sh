f55a0eb37eeca11342e552a0edbc3865ef2a941c
hlx821128 <hlx821128@qq.com>
2024-06-16 00:36:45 +0800

    add BLOCK ps name

--- launch_env.sh
+++ launch_env.sh
@@ -6,6 +6,7 @@ export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
+export BLOCK='uploader,loggerd,mapd'
 export ATHENA_HOST='ws://op.caminfo.cn:7776'
 export API_HOST='http://op.caminfo.cn:7777'
 export MAPBOX_TOKEN='sk.eyJ1IjoibWF3ZWl5dXdlaXdlaSIsImEiOiJjbG15NHN1dDMwdWc5MmxwaDdkZ3Z5dHNyIn0.pk06qTKkAZyBC1Z37v8i0A'
