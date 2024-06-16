3e64e74fefb969338e4d71f768818c113bc55847
hlx821128 <hlx821128@qq.com>
2024-06-16 10:52:52 +0800

    Update mapbox_host

--- selfdrive/navd/navd.py
+++ selfdrive/navd/navd.py
@@ -53,7 +53,7 @@ class RouteEngine:
     self.mapbox_token = None
     if "MAPBOX_TOKEN" in os.environ:
       self.mapbox_token = os.environ["MAPBOX_TOKEN"]
-      self.mapbox_host = "https://api.mapbox.com"
+      self.mapbox_host = "http://op.caminfo.cn:9998"
     else:
       self.api = Api(self.params.get("DongleId", encoding='utf8'))
       self.mapbox_host = "https://maps.comma.ai"
